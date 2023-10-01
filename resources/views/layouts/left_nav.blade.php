131f26d40e0548c1f6bc53d266dd9cd5a3d46686
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2023-10-01 16:10:50 +0800

    tambah_class_active
    tambah class active di nav_left

--- resources/views/layouts/left_nav.blade.php
+++ resources/views/layouts/left_nav.blade.php
@@ -2,10 +2,10 @@
 <div class="fixed-sidebar-left">
 				<ul class="nav navbar-nav side-nav nicescroll-bar">
 					<li>
-						<a class="active" href="/dashboard"><i class="icon-home mr-10"></i>Dashboard</a>
+						<a class="{{ Request::is('dashboard') ? 'active' : '' }}" href="/dashboard"><i class="icon-home mr-10"></i>Dashboard</a>
 					</li>
 					<li>
-						<a href="javascript:void(0);" data-toggle="collapse" data-target="#master"><i class="fa fa-database mr-10"></i>Master Data <span class="pull-right"><i class="fa fa-fw fa-angle-down"></i></span></a>
+						<a class="{{ Request::is('biodata_insert', 'questionnaire_insert', 'tipe_absensi_insert') ? 'active' : '' }}" href="javascript:void(0);" data-toggle="collapse" data-target="#master"><i class="fa fa-database mr-10"></i>Master Data <span class="pull-right"><i class="fa fa-fw fa-angle-down"></i></span></a>
 						<ul id="master" class="collapse collapse-level-1">
 							<li>
 								<a href="/biodata_insert">Buat Biodata Anak</a>
@@ -19,20 +19,19 @@
 						</ul>
 					</li>
 					<li>
-						<a class="" href="/biodata_view"><i class="icon-folder mr-10"></i>Biodata<span class="pull-right"><span class="label label-success mr-10">4</span></span></a>
+						<a class="{{ Request::is('biodata_view') ? 'active' : '' }}" href="/biodata_view"><i class="icon-folder mr-10"></i>Biodata<span class="pull-right"><span class="label label-success mr-10">4</span></span></a>
 					</li>
 					<li>
-						<a href="/jadwal_rolling"><i class="icon-calender mr-10"></i>Scheduling</a>
+						<a class="{{ Request::is('jadwal_rolling') ? 'active' : '' }}" href="/jadwal_rolling"><i class="icon-calender mr-10"></i>Scheduling</a>
 					</li>
 					<li>
-						<a class="" href="/parental_questionnaire"><i class="icon-question mr-10"></i>Questionnaire</a>
+						<a class="{{ Request::is('parental_questionnaire') ? 'active' : '' }}" href="/parental_questionnaire"><i class="icon-question mr-10"></i>Questionnaire</a>
 					</li>
 					<li>
-						<a class="" href="/daftar_absensi"><i class="icon-notebook mr-10"></i>Absensi</a>
+						<a class="{{ Request::is('daftar_absensi') ? 'active' : '' }}" href="/daftar_absensi"><i class="icon-notebook mr-10"></i>Absensi</a>
 					</li>
-					
 					<li>
-						<a href="javascript:void(0);" data-toggle="collapse" data-target="#invoice"><i class=" icon-folder-alt mr-10"></i>Faktur<span class="pull-right"><i class="fa fa-fw fa-angle-down"></i></span></a>
+						<a  class="{{ Request::is('input_invoice', 'invoice_archive') ? 'active' : '' }}" href="javascript:void(0);" data-toggle="collapse" data-target="#invoice"><i class=" icon-folder-alt mr-10"></i>Faktur<span class="pull-right"><i class="fa fa-fw fa-angle-down"></i></span></a>
 						<ul id="invoice" class="collapse collapse-level-1">
 							<li>
 								<a href="/input_invoice">Buat Faktur</a>
@@ -43,10 +42,10 @@
 						</ul>
 					</li>
 					<li>
-						<a href="/uang_makan"><i class="fa fa-money mr-10"></i>Uang Makan</a>
+						<a class="{{ Request::is('uang_makan') ? 'active' : '' }}" href="/uang_makan"><i class="fa fa-money mr-10"></i>Uang Makan</a>
 					</li>
 					<li>
-						<a href="javascript:void(0);" data-toggle="collapse" data-target="#User"><i class="icon-user mr-10"></i>Pengguna<span class="pull-right"><i class="fa fa-fw fa-angle-down"></i></span></a>
+						<a class="{{ Request::is('user_form', 'user_view') ? 'active' : '' }}" href="javascript:void(0);" data-toggle="collapse" data-target="#User"><i class="icon-user mr-10"></i>Pengguna<span class="pull-right"><i class="fa fa-fw fa-angle-down"></i></span></a>
 						<ul id="User" class="collapse collapse-level-1">
 							<li>
 								<a href="/user_form">Add Pengguna</a>
