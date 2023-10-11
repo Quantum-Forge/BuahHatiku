ea13cd35b656cf57a1b2322e3d0ae37ee0f33f51
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2023-10-11 21:49:19 +0800

    ubah_locale_daftar_absensi_picker

--- public/dist/js/daftar_absensi_picker.js
+++ public/dist/js/daftar_absensi_picker.js
@@ -2,64 +2,15 @@
 
 $(document).ready(function() {
 	"use strict";
+		/* Daterange picker Init*/
+		$('#Tanggal').daterangepicker({
+			buttonClasses: ['btn', 'btn-sm'],
+				   applyClass: 'btn-info',
+				   cancelClass: 'btn-default',
+				   locale: {
+					   format: 'DD/MM/YYYY',
+					   language: 'id'
+				   }
+		   });
 	
-
-	 
-	// $('#datetimepicker3').datetimepicker({
-	// 		format: 'DD-MM-YYYY',
-	// 		inline:true,
-	// 		sideBySide: true,
-	// 		icons: {
-    //                 time: "fa fa-clock-o",
-    //                 date: "fa fa-calendar",
-    //                 up: "fa fa-arrow-up",
-    //                 down: "fa fa-arrow-down"
-    //             },
-	// }); 
-
-	// $('#datetimepicker4').datetimepicker({
-	// 		inline:true,
-	// 		sideBySide: true,
-	// 		useCurrent: false,
-	// 		icons: {
-    //                 time: "fa fa-clock-o",
-    //                 date: "fa fa-calendar",
-    //                 up: "fa fa-arrow-up",
-    //                 down: "fa fa-arrow-down"
-    //             },
-	// }).data("DateTimePicker").date(moment());
-	
-	/* Daterange picker Init*/
-	var today = new Date();
-	var threeDaysAgo = new Date(today.getTime() - 3 * 24 * 60 * 60 * 1000);
-
-	$('#Tanggal').daterangepicker({
-	startDate: threeDaysAgo,
-	endDate: today,
-	format:'DD/MM/YYYY',
-	buttonClasses: ['btn', 'btn-sm'],
-	applyClass: 'btn-info',
-	cancelClass: 'btn-default'
-	});
-	// $('.input-daterange-timepicker').daterangepicker({
-	// 	timePicker: true,
-	// 	format: 'MM/DD/YYYY h:mm A',
-	// 	timePickerIncrement: 30,
-	// 	timePicker12Hour: true,
-	// 	timePickerSeconds: false,
-	// 	buttonClasses: ['btn', 'btn-sm'],
-	// 	applyClass: 'btn-info',
-	// 	cancelClass: 'btn-default'
-	// });
-	// $('#Tanggal').daterangepicker({
-	// 	format: 'MM/DD/YYYY',
-	// 	minDate: 'threeDaysAgo',
-	// 	maxDate: '06/30/2015',
-	// 	buttonClasses: ['btn', 'btn-sm'],
-	// 	applyClass: 'btn-info',
-	// 	cancelClass: 'btn-default',
-	// 	dateLimit: {
-	// 		days: 6
-	// 	}
-	// });
 });
