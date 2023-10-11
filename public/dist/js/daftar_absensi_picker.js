8e52e897feba1180bbfb792e60480082f26ecddb
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2023-10-11 21:54:07 +0800

    update_3hari_sebelum

--- public/dist/js/daftar_absensi_picker.js
+++ public/dist/js/daftar_absensi_picker.js
@@ -2,8 +2,12 @@
 
 $(document).ready(function() {
 	"use strict";
+	var today = new Date();
+	var threeDaysAgo = new Date(today.getTime() - 3 * 24 * 60 * 60 * 1000);
 		/* Daterange picker Init*/
 		$('#Tanggal').daterangepicker({
+			startDate: threeDaysAgo,
+			endDate: today,
 			buttonClasses: ['btn', 'btn-sm'],
 				   applyClass: 'btn-info',
 				   cancelClass: 'btn-default',
