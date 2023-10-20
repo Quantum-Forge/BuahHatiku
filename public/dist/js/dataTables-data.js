c8286d94b46a811e958f78b5a07e7085096ff300
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2023-10-20 22:27:50 +0800

    minify_dataTables.js

--- public/dist/js/dataTables-data.js
+++ public/dist/js/dataTables-data.js
@@ -4,10 +4,7 @@
 
 $(document).ready(function() {
 	"use strict";
-	$('#user_view').DataTable({
-		paging: true,
-		ordering: false,
-	});
+	$('#user_view').DataTable({paging: true,ordering: false});
 	$('#datable_1').DataTable();
     $('#datable_2').DataTable({ "lengthChange": false});
 } );
