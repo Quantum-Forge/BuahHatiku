4dfd1b4cc212a88503a76838697e8cb6df1177a2
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2023-10-23 00:31:05 +0800

    coba_ubah_format_date

--- public/dist/js/jadwal_rolling-edit.js
+++ public/dist/js/jadwal_rolling-edit.js
@@ -2,20 +2,21 @@
 
 $(document).ready(function() {
 	"use strict";
-	var today = new Date(); // Get the current date and time
-	var sixDaysLater = new Date(today.getTime() + 6 * 24 * 60 * 60 * 1000); // Calculate 7 days in the future
 	/* Datetimepicker Init*/
-	$('#Tanggal').daterangepicker({
-		startDate: today,
-		endDate: sixDaysLater,
-		buttonClasses: ['btn', 'btn-sm'],
-			   applyClass: 'btn-info',
-			   cancelClass: 'btn-default',
-			   locale: {
-				   format: 'DD/MM/YYYY',
-				   language: 'id'
-			   }
-	   });
+	$('#Tanggal').datetimepicker({
+		format: 'DD/MM/YYYY',
+		useCurrent: true,
+		icons: {
+			time: "fa fa-clock-o",
+			date: "fa fa-calendar",
+			up: "fa fa-arrow-up",
+			down: "fa fa-arrow-down"
+		}
+	}).on('dp.change', function(e) {
+		if (e.date === null) {
+			$(this).data("DateTimePicker").date(moment());
+		}
+	});
 
 	$('#WaktuMulai').datetimepicker({
 		format: 'HH:mm',
