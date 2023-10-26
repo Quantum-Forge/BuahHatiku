dd21ec6e8a11656d4e8274e7870e97741a94f740
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2023-10-26 16:52:46 +0800

    minify_data_table.js

--- public/dist/js/dataTables-data.js
+++ public/dist/js/dataTables-data.js
@@ -1,11 +1,13 @@
 /*DataTable Init*/
 
-"use strict"; 
+// "use strict"; 
 
-$(document).ready(function() {
-	"use strict";
-	$('#jadwal_table').DataTable({"lengthMenu": [ [5, 25, 50, -1], [5, 25, 50, "All"] ],"pageLength": 5,ordering: false});
-	$('#user_view').DataTable({paging: true,ordering: false});
-	$('#datable_1').DataTable();
-    $('#datable_2').DataTable({ "lengthChange": false});
-} );
+// $(document).ready(function() {
+// 	"use strict";
+// 	$('#jadwal_table').DataTable({"lengthMenu": [ [5, 25, 50, -1], [5, 25, 50, "All"] ],"pageLength": 5,ordering: false});
+// 	$('#user_view').DataTable({paging: true,ordering: false});
+// 	$('#datable_1').DataTable();
+//     $('#datable_2').DataTable({ "lengthChange": false});
+// } );
+
+"use strict";$(document).ready((function(){$("#jadwal_table").DataTable({lengthMenu:[[5,25,50,-1],[5,25,50,"All"]],pageLength:5,ordering:!1}),$("#user_view").DataTable({paging:!0,ordering:!1}),$("#datable_1").DataTable(),$("#datable_2").DataTable({lengthChange:!1})}));
