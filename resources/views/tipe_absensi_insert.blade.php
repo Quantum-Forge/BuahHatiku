1e3706be6176e0de3486902286df6c251d8e7380
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2023-11-10 16:38:29 +0800

    tipe_absensi_bug
    - tipe absensi ga bisa keedit, satu-satu

--- resources/views/tipe_absensi_insert.blade.php
+++ resources/views/tipe_absensi_insert.blade.php
@@ -126,7 +126,6 @@
 																	<div class="modal-body">
 																		<div class="form-group ">
 																			<div class="">
-																			<label for="" class="control-label">JenisAbsensi*</label>
 																				<div class="input-group">
 																					<div class="input-group-addon"><i class="icon-list"></i></div>
 																					<input type="text" required placeholder="Isi Jenis..." name="JenisAbsensi" class="form-control" value="{{$tipe_absensi->JenisAbsensi}}">
@@ -135,25 +134,19 @@
 																		</div>
 																		<div class="form-group">
 																			<div class="">
-																				<label for="" class="control-label">Harga*</label>
 																				<div class="input-group">
 																					<div class="input-group-addon">Rp.</div>
-																					<input type="number" required name="Harga" placeholder="Isi Harga..." class="form-control"  value="{{$tipe_absensi->Harga}}">
+																					<input type="number" name="Harga" placeholder="Isi Harga..." class="form-control"  value="{{$tipe_absensi->Harga}}">
 																				</div>
 																			</div>
 																		</div>
 																		<div class="form-group">
-																			<div class="">
-																				<label for="" class="control-label">Durasi*</label>
-																				<input placeholder="Isi Sesi..." name="Durasi" class="vertical-spin form-control" type="number" data-bts-button-down-class="btn btn-default" data-bts-button-up-class="btn btn-default" value="{{$tipe_absensi->Durasi}}" style="display: block;">
-																				<span class="input-group-addon bootstrap-touchspin-postfix" style="display: none;"></span>
-																			</div>
+																			<input placeholder="Isi Sesi..." name="Durasi" class="form-control" type="number" value="{{$tipe_absensi->Durasi}}">
 																		</div>
 																		<div class="form-group">
 																			<div class="">
-																				<label for="" class="control-label">Keterangan*</label>
 																				<div class="input-group">
-																					<textarea name="Keterangan" required placeholder="Isi Keterangan..." class="form-control" id="" cols="100" rows="10">{{$tipe_absensi->Keterangan}}</textarea>
+																					<textarea name="Keterangan" placeholder="Isi Keterangan..." class="form-control" id="" cols="100" rows="10">{{$tipe_absensi->Keterangan}}</textarea>
 																				</div>
 																			</div>
 																		</div>
