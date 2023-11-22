a372a0deba77be2e0ae670b9fb2a4b04be3f2e93
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2023-11-22 21:25:45 +0800

    set(daftarabsensi): change format

--- public/dist/js/daftar_absensi_picker.js
+++ public/dist/js/daftar_absensi_picker.js
@@ -4,19 +4,10 @@ $(document).ready(function() {
 	"use strict";
 	var today = new Date();
 	// Set startDate berdasarkan bulan berjalan
+	var today = new Date();
 	var startOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+	var lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
 
-    var targetYear = today.getFullYear(); // Tahun target awal
-    var targetMonth = 5; // 5 merepresentasikan bulan Juni (mulai dari 0)
-    var targetDay = 30;
-
-    // Cek apakah tanggal hari ini sudah mencapai atau melampaui 30 Juni di tahun ini
-    if (today.getMonth() > targetMonth || (today.getMonth() === targetMonth && today.getDate() >= targetDay)) {
-        // Jika iya, tambahkan satu tahun
-        targetYear++;
-    }
-
-    var lastDayOfMonth = new Date(targetYear, targetMonth, targetDay);
 		/* Daterange picker Init*/
 		$('#Tanggal').daterangepicker({
 			startDate: startOfMonth,
