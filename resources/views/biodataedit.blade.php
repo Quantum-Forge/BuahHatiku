acf1c0ebb2a6b570d5d98dc647daefaaf27a558f
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2024-05-07 00:19:41 +0800

    fix: change index.html to /dashboard

--- resources/views/biodataedit.blade.php
+++ resources/views/biodataedit.blade.php
@@ -10,7 +10,7 @@
 	<!-- Breadcrumb -->
 	<div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
 		<ol class="breadcrumb">
-			<li><a href="index.html">Dashboard</a></li>
+			<li><a href="/dashboard">Dashboard</a></li>
 			<li class="active"><span>Biodata</span></li>
 		</ol>
 	</div>
