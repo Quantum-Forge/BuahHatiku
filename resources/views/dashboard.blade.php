db48393989dd26ebfd46611a9f56beedcdf45733
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2024-05-24 17:11:37 +0800

    refactor: Add conditional check for user and biodata existence
    Added conditional check to ensure that both user and biodata exist before displaying appointment details. This prevents potential errors when accessing related data.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -134,28 +134,17 @@
 										<th>status</th>
 									</tr>
 								</thead>
-								{{-- looping 4x sesuai urutan upload --}}
 								<tbody>
 									@foreach($jadwals as $jadwal)
-									<tr>
-										<td>{{$jadwal->Tanggal}}</td>
-										<td>{{$jadwal->WaktuMulai.' - '.$jadwal->WaktuSelesai}}</td>
-										<td>
-											@if($jadwal->user)
-												{{$jadwal->user->Nama}}
-											@else
-												<span class="label label-danger">Not Found</span>
-											@endif	
-										</td>
-										<td>
-											@if($jadwal->biodata)
-												{{$jadwal->biodata->Nama}}
-											@else
-												<span class="label label-danger">Not Found</span>
-											@endif
-										</td>										
-										<td><span class="label {{$jadwal->absensi->Hadir == 1 ? 'label-success' : 'label-danger'}} font-weight-100">{{$jadwal->absensi->Hadir == 1 ? 'Hadir' : 'Tidak Hadir'}}</span></td>
-									</tr>
+										@if($jadwal->user && $jadwal->biodata)
+											<tr>
+												<td>{{$jadwal->Tanggal}}</td>
+												<td>{{$jadwal->WaktuMulai.' - '.$jadwal->WaktuSelesai}}</td>
+												<td>{{$jadwal->user->Nama}}</td>
+												<td>{{$jadwal->biodata->Nama}}</td>                                        
+												<td><span class="label {{$jadwal->absensi->Hadir == 1 ? 'label-success' : 'label-danger'}} font-weight-100">{{$jadwal->absensi->Hadir == 1 ? 'Hadir' : 'Tidak Hadir'}}</span></td>
+											</tr>
+										@endif
 									@endforeach
 								</tbody>
 							</table>
